301474aa100d5c8c2ef5a8cde0829ab5778366d5
misterZink <heidarirobin@protonmail.com>
2021-01-27 14:54:16 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
 
-    
+
     public static void main(String[] args) {
-         Library.getLibrary();
-         Book book = new Book("Fever", "John Lennon", "Destopian", "#1234");
+        Library.getLibrary();
+        Book book = new Book("Fever", "John Lennon", "Destopian", "#1234");
 
         Author author = new Author("John", "Lennon");
         author.addToList(book);
-         author.showList();
+        author.showList();
+
     }
 
 }
