395bf3d32f70b7ffded19641df2e43bfc97755e2
ziggabyte <ziggi.willpower@gmail.com>
2021-01-28 11:38:48 +0100

    first version of login, with users as hashmap

--- src/Program.java
+++ src/Program.java
@@ -1,68 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Program {
-    private List<User> users = new ArrayList<>();   //testformat för att spara alla Users
+    private HashMap<String, User> users = new HashMap<>();   //testformat för att spara alla Users
 
     private void initiateTestUsers() {
-        users.add(new User());                  //tomma Users för att testa
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
+        int counter = 1;
 
-        int counter = 1;                        //sätter username och password för alla users
-        for (User u : users) {
-            u.setUsername("user" + counter);
-            u.setPassword("password" + counter);
-            counter++;
+        while (counter < 10) {
+            users.put("user" + counter, new User());
+            users.get("user" + counter).setPassword("password" + counter);
+            counter ++;
         }
     }
 
     public void start() {
-        initiateTestUsers();
+        initiateTestUsers(); // skapar testanvändare
 
         User currentUser = null;
         Scanner scan = new Scanner(System.in);
-        String usernameInput = "";
-        String passwordInput = "";
+        String usernameInput;
+        String passwordInput;
         boolean wasFound = false;
         int loginAttempts = 0;
 
         do {
             System.out.println("Username: ");       // Läser in username
             usernameInput = scan.nextLine();
-            for (User user : users) {               // Kollar input mot alla usernames
-                if (user.getUsername().equals(usernameInput)) {
-                    currentUser = user;             // Sätter currentUser ifall username finns
-                }
+            if (users.containsKey(usernameInput)) {
+                currentUser = users.get(usernameInput);
             }
             loginAttempts ++;
+
             if (currentUser == null && loginAttempts >= 10) {
                 System.out.println("Det verkar som att du glömt ditt användarnamn. Kontakta kundtjänst.");
-                //sen: kör metod som avslutar programmet.?
+                System.exit(0); // fult avslut, vill avsluta snyggare här
             }
             else if (currentUser == null) System.out.println("Användarnamnet finns inte, försök igen.");
+        } while (currentUser == null);
 
-        } while (currentUser == null && loginAttempts < 10);
+        loginAttempts = 0;
 
         do {
             System.out.println("Password:");        // läser in password
             passwordInput = scan.nextLine();
+
             if (currentUser.getPassword().equals(passwordInput)) { // matchar mot currentUser's password
                 wasFound = true;
             }
-            if (!wasFound) System.out.println("Fel lösenord, försök igen.");
+            loginAttempts++;
+            if (!wasFound && loginAttempts >= 10) {
+                System.out.println("Du verkar ha glömt ditt lösenord, kontakta kundtjänst");
+                System.exit(0); // fult avslut, vill avsluta snyggare här
+            }
+            else if (!wasFound) System.out.println("Fel lösenord, försök igen.");
         } while (!wasFound);
 
-        System.out.println("Välkommen till biblioteket, " + currentUser.getUsername());
+        System.out.println("Välkommen till biblioteket");
     }
-
-    public static void main(String[] args) {
-        Program program = new Program();
-        program.start();
-    }
-
 }
